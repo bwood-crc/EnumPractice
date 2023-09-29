e5a136009688e6172a49ac9b508aa0d4c8991551
Brendan Wood <54809451+bwood-crc@users.noreply.github.com>
2023-09-29 10:51:55 -0400

    Add project files.

---
+++ EnumPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFile file1 = new MyFile("picture.txt",
+                @"C:\users\bwood\",
+                Rights.READABLE | Rights.WRITABLE,
+                null);
+
+            // Is file1 readable?
+
+            Console.WriteLine(file1.FileRights);
+
+
+        }
+    }
+}
